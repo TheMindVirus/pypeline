3344faf633af38f4e6e9ea42a99691f659659e07
Alastair Cota <a1.cota.365@gmail.com>
2023-11-29 04:16:34 +0000

    Added Dev Branch to Nightfall

--- dev/README.md
+++ dev/README.md
@@ -4,3 +4,4 @@
 ![tension](https://github.com/themindvirus/pypeline/blob/nightfall/dev/tension.png)
 ![arab](https://github.com/themindvirus/pypeline/blob/nightfall/dev/arab.png)
 ![porta](https://github.com/themindvirus/pypeline/blob/nightfall/dev/porta.png)
+![slew](https://github.com/themindvirus/pypeline/blob/nightfall/dev/slew.png)
--- dev/wavetable.py
+++ dev/wavetable.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as mpl
 
 TITLE = "Colour Map"
 
-def porta(x, amplitude = 1, lift = 0, frequency = 1, phase = 0):
+def porta(x, frequency = 1, phase = 0, amplitude = 1, lift = 0, clamp = 1):
     y = 0
     x = x * frequency + phase
     x = x % 1.0
@@ -23,32 +23,98 @@ def porta(x, amplitude = 1, lift = 0, frequency = 1, phase = 0):
     else:
         y = 1
     y = y * amplitude + lift
+    if clamp:
+        y = min(max(y, 0.0), 1.0)
     return y
 
-def slew():
-    pass
+def slew(x, tiers = 3, frequency = 1, phase = 0, amplitude = 1, lift = 0, clamp = 0):
+    y = 0
+    x = x * frequency + phase
+    x = x % 1.0
+
+    if x > 0.5:
+        x = 1 - x
+    tiers = max(0, tiers)
+    if tiers < 2:
+        return tiers
+    n = 4 * tiers - 2
+    steps = tiers - 1
+    step = 1 / steps
+    tier = 0
+    for i in range(1, n):
+        k = i / n
+        if x <= k:
+            if i % 2 == 0:
+                y = tier + ((math.cos((math.pi*(x + k)*n))+1)/2) / steps
+            else:
+                y = tier
+            break
+        if i % 2 == 0:
+            tier += step
+        
+    y = y * amplitude + lift
+    if clamp:
+        y = min(max(y, 0.0), 1.0)
+    return y
+
+def custom(x, p):
+    if p == 1:
+        if x <= (1/6):
+            return 1
+        if x <= (2/6):
+            return slew(x, frequency = 3)
+        if x <= (4/6):
+            return 0
+        if x <= (5/6):
+            return slew(x, frequency = 3)
+        if x <= (6/6):
+            return 1
+    if p == 2:
+        if x <= (1/6):
+            return slew(x, frequency = 3)
+        if x <= (3/6):
+            return 1
+        if x <= (4/6):
+            return slew(x, frequency = 3)
+        if x <= (6/6):
+            return 0
+    if p == 3:
+        if x <= (2/6):
+            return 0
+        if x <= (3/6):
+            return slew(x, frequency = 3)
+        if x <= (5/6):
+            return 1
+        if x <= (6/6):
+            return slew(x, frequency = 3)
+    return 0
 
 def main():
     mpl.style.use("dark_background")
     x = np.linspace(0.0, 1.0, 100)
     y = []
-    p = 2
 
     y_Red = method()
     y_Red.label = "Red"
-    y_Red.method = [x, [porta(x, phase = (0/3), amplitude = 0.5, lift = 0.25) for x in x]]
+    #y_Red.method = [x, [porta(x, phase = (0/3)) for x in x]]
+    #y_Red.method = [x, [1 - slew(x, phase = (0/3)) for x in x]]
+    y_Red.method = [x, [custom(x, 1) for x in x]]
     y_Red.colour = (1.0, 0.0, 0.3)
     y.append(y_Red)
-
+    
     y_Green = method()
     y_Green.label = "Green"
-    y_Green.method = [x, [porta(x, phase = (1/3)) for x in x]]
+    #y_Green.method = [x, [porta(x, phase = (1/3)) for x in x]]
+    #y_Green.method = [x, [1 - slew(x, phase = (1/3)) for x in x]]
+    y_Green.method = [x, [custom(x, 2) for x in x]]
     y_Green.colour = (0.3, 1.0, 0.0)
     y.append(y_Green)
 
     y_Blue = method()
     y_Blue.label = "Blue"
-    y_Blue.method = [x, [porta(x, phase = (2/3)) for x in x]]
+    #y_Blue.method = [x, [porta(x, phase = (2/3)) for x in x]]
+    #y_Blue.method = [x, [1 - slew(x, phase = (2/3)) for x in x]]
+    y_Blue.method = [x, [custom(x, 3) for x in x]]
     y_Blue.colour = (0.0, 0.3, 1.0)
     y.append(y_Blue)
     
